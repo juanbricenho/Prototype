d2d481c76ed5a87c0a42b9e5a5a91a86e86be5d5
Juan Lopez-Briceño <j1312z5@gmail.com>
2016-09-10 01:45:58 -0500

    File foods Allergy-preferred
    base1

---
+++ desirableFoods.pl
@@ -0,0 +1,13 @@
+alergia('PASTAS').
+
+preferente('CARNES').
+preferente('CEREALES').
+preferente('FRUTAS').
+preferente('HUEVOS').
+preferente('LACTEOS').
+preferente('LEGUMINOSAS').
+preferente('VERDURAS').
+preferente('SOPAS').
+
+
+
